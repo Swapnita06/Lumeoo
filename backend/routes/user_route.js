03a696fdb9fd73e575f719a2ec3e4c01c15bde42
swapnita <swapimamuni@gmail.com>
2024-10-22 10:40:08 +0530

    login backend done

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -5,6 +5,8 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config()
 const User = require('../models/User');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
@@ -12,6 +14,8 @@ cloudinary.config({
     api_secret: process.env.API_SECRET 
 });
 
+
+//this is for signup
 Router.post('/signup', async(req,res)=>{
     try{
         const users =  await User.find({email:req.body.email})
@@ -44,6 +48,61 @@ Router.post('/signup', async(req,res)=>{
         console.log(err)
         res.status(500).json({
             error:err
+        })
+    }
+})
+
+
+//this is for login.
+Router.post('/login',async(req,res)=>{
+    console.log(req.body)
+    try{
+        console.log(req.body)
+        const users = await User.find({email:req.body.email})
+        console.log(users)
+        if(users.length==0){
+            return res.status(500).json({
+                error:'email is not registered...'
+            })
+        }
+
+const isValid = await bcrypt.compare(req.body.password,users[0].password)
+console.log(isValid)
+if(!isValid){
+    return res.status(500).json({
+        error:'invalid password'
+    })
+}
+
+const token = jwt.sign({
+    _id:users[0]._id,
+    channelName:users[0].channelName,
+    email:users[0].email,
+    phone:users[0].phone,
+    logoId:users[0].logoId
+},
+'swapnita singh',
+{
+    expiresIn:'365d'
+}
+)
+res.status(200).json({
+    _id:users[0]._id,
+    channelName:users[0].channelName,
+    email:users[0].email,
+    phone:users[0].phone,
+    logoId:users[0].logoId,
+    logoUrl:users[0].logoUrl,
+    token:token,
+    subscribers:users[0].subscribers,
+    subscribedChannels:users[0].subscribedChannels
+})
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({
+            error:'something is wrong!'
         })
     }
 })
